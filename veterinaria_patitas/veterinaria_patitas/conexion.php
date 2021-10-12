753ad00d59837167b6f4b396aedaee4cd5b0c830
RomiMolis8 <rominamolina_4@hotmail.com>
2021-10-12 18:22:39 -0300

    web

---
+++ veterinaria_patitas/veterinaria_patitas/conexion.php
@@ -0,0 +1,8 @@
+<?php
+
+// datos Xampp
+$datos_base=mysqli_connect("localhost", "id17593546_root", "A]#Mpm(1Oy-|]mFw", "id17593546_veterinaria");
+// mysqli_connect(servidor_mysql, usuario_mysql, clave_mysql, nombre_base);
+
+
+?>
---
+++ veterinaria_patitas/veterinaria_patitas/enviar_datos.php
@@ -0,0 +1,25 @@
+<?php
+$nombre_formu=$_POST['nombre_apellido'];
+$correo_formu=$_POST['correo'];
+$tel_formu=$_POST['tel'];
+$motivo_formu=$_POST['motivo'];
+$mensaje_formu=$_POST['mensaje'];
+
+$destino="rominamolina_4@hotmail.com";
+
+$asunto="Mensaje desde la pagina Veterinaria";
+
+$cuerpo="Nombre: ".$nombre_formu."\r\n"."Correo: ".$correo_formu."\r\n"."Tel: ".$tel_formu."\r\n"."Motivo: ".$motivo_formu."\r\n"."Mensaje: ".$mensaje_formu;
+
+$remitente="From:<$correo_formu>";
+
+mail($destino,$asunto,$cuerpo,$remitente);
+
+include("conexion.php");
+
+mysqli_query($datos_base, "INSERT INTO mensajes VALUES (0, '$nombre_formu', '$correo_formu', $tel_formu, '$motivo_formu', '$mensaje_formu')");
+
+header("Location: mensaje_envio.php");
+
+
+?>
