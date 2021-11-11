e1bb1073135842d203b8db3b038d654aed11fb69
RomiMolis8 <rominamolina_4@hotmail.com>
2021-11-11 08:24:36 -0300

    Actualizacion de carousel y doc IEEE830

--- veterinaria_patitas/veterinaria_patitas/conexion.php
+++ veterinaria_patitas/veterinaria_patitas/conexion.php
@@ -1,8 +1,17 @@
 <?php
 
 // datos Xampp
-$datos_base=mysqli_connect("localhost", "id17593546_root", "A]#Mpm(1Oy-|]mFw", "id17593546_veterinaria");
+//$datos_base=mysqli_connect("localhost", "id17593546_root", "A]#Mpm(1Oy-|]mFw", "id17593546_veterinaria");
 // mysqli_connect(servidor_mysql, usuario_mysql, clave_mysql, nombre_base);
 
+ $host="localhost";
+ $usuario="id17593546_admin";
+ $clave="V>J?c3Enn0Ba!WC+";
+ $basededatos="id17593546_veterinariapatitas";
 
+ $conn = new mysqli($host,  $usuario,  $clave,  $basededatos);
+    mysqli_query($conn , "SET character_set_result=utf8");
+    if($conn->connect_error){
+        die("Error de base de datos : " .  $conn->connect_error);
+    }
 ?>
--- veterinaria_patitas/veterinaria_patitas/enviar_datos.php
+++ veterinaria_patitas/veterinaria_patitas/enviar_datos.php
@@ -1,9 +1,9 @@
 <?php
-$nombre_formu=$_POST['nombre_apellido'];
-$correo_formu=$_POST['correo'];
-$tel_formu=$_POST['tel'];
-$motivo_formu=$_POST['motivo'];
-$mensaje_formu=$_POST['mensaje'];
+$nombre_formu=$_POST["nombre_apellido"];
+$correo_formu=$_POST["correo"];
+$tel_formu=$_POST["tel"];
+$motivo_formu=$_POST["motivo"];
+$mensaje_formu=$_POST["mensaje"];
 
 $destino="rominamolina_4@hotmail.com";
 
@@ -13,9 +13,32 @@ $cuerpo="Nombre: ".$nombre_formu."\r\n"."Correo: ".$correo_formu."\r\n"."Tel: ".
 
 $remitente="From:<$correo_formu>";
 
-@mail($destino,$asunto,$cuerpo,$remitente);
+mail($destino,$asunto,$cuerpo,$remitente);
 
-/*include("conexion.php");
+include("conexion.php");
+ $conn = mysqli_connect($host,$usuario,$clave,$basededatos) o die ("No se ha podido conectar al servidor de Base de datos");
+si (! $conn) {
+    die("Conexión fallida: " . mysqli_connect_error());
+}
+
+
+
+ $db = mysqli_select_db($con,  $basededatos) o morir ( "Upps! no se ha podido conectar a la base de datos" );
+ $consulta = "INSERT INTO usuarios (nombreapellido, correo, telefono, motivo, mensaje) VALUES ('$nombre_formu', '$correo_formu', '$tel_formu', '$motivo_formu', '$mensaje_formu')";
+
+Usamos esas variables:
+if (mysqli_query ($con,  $consulta)){
+    eco "<h3>Registro agregado.</h3>";
+    } más {
+    echo "<h3>No se agregó nuevo registro</h3>";
+    echo "Error: " .  $consulta . "<br>" . mysqli_error($con);
+ }
+ mysqli_close($con);
+
+
+
+
+include("conexion.php");
 
 mysqli_query($datos_base, "INSERT INTO mensajes VALUES (0, '$nombre_formu', '$correo_formu', $tel_formu, '$motivo_formu', '$mensaje_formu')");*/
 
